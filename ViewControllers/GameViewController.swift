e701d01f1875e568f69c0617dd7ae4a56bf6ec4e
Sergey-Kosichkin <d978827@icloud.com>
2021-04-19 06:46:05 +0300

    Initial commit

---
+++ ViewControllers/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  wordsGameApp
+//
+//  Created by Сергей Косичкин on 19.04.2021.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
