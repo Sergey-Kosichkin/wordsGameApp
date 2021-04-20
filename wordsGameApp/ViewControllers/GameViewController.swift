d7e130d12b9d39f07e42238ff26cb31e30213325
AnnGolub <anka.golub17@gmail.com>
2021-04-20 16:45:37 +0300

    Add Table view controller, change main screen

--- wordsGameApp/ViewControllers/GameViewController.swift
+++ wordsGameApp/ViewControllers/GameViewController.swift
@@ -8,20 +8,26 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
     @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var helpDescriptionLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var helpButton: UIButton!
+    @IBOutlet weak var changeModeButton: UIButton!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        backgroundImage.image = UIImage(named: "Mask Group")
+        view.bringSubviewToFront(startButton)
+        startButton.layer.cornerRadius = 18
+        
         
     }
-
+    
     @IBAction func startButtonPressed() {
     }
     
-
     @IBAction func helpButtonPressed() {
     }
 }
