d644ffe878ceb60938bb0ba15bce050493dc59c0
AnnGolub <anka.golub17@gmail.com>
2021-04-21 21:07:58 +0300

    New element on storyboard, methods with keyboard

--- wordsGameApp/ViewControllers/GameViewController.swift
+++ wordsGameApp/ViewControllers/GameViewController.swift
@@ -11,6 +11,7 @@ class GameViewController: UIViewController {
     
     @IBOutlet weak var answerTextField: UITextField!
     @IBOutlet weak var helpDescriptionLabel: UILabel!
+    @IBOutlet weak var lastAnswerLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var helpButton: UIButton!
     @IBOutlet weak var changeModeButton: UIButton!
@@ -20,17 +21,38 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         backgroundImage.image = UIImage(named: "Mask Group")
         view.bringSubviewToFront(startButton)
+        
         startButton.layer.cornerRadius = 18
         
-        
+        helpDescriptionLabel.isHidden.toggle()
+        lastAnswerLabel.isHidden.toggle()
+        answerTextField.placeholder = "Введите слово на последнюю букву 'буква'"
+
     }
     
     @IBAction func startButtonPressed() {
         doneButtonAction(from: answerTextField.text ?? "")
+        lastAnswerLabel.isHidden = false
+        lastAnswerLabel.text = answerTextField.text
     }
     
     @IBAction func helpButtonPressed() {
+        helpDescriptionLabel.isHidden = false
         helpButtonAction(from: answerTextField.text ?? "")
     }
 }
 
+// MARK: - Work with keyboard
+extension GameViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == answerTextField {
+            startButtonPressed()
+        }
+        return true
+    }
+}
